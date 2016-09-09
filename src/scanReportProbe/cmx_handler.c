90f8f0504ea2e6e28ec993ec7876bd66f0edf0e6
PP90 <pietropiscione@gmail.com>
2016-09-09 23:10:48 +0200

    Starting implementation of http post of file
    The implementatation of HTTP POST of the JSON file has been started. It
    looks like doesn't work 'cause because it gets a DNS error. To be fixed

--- src/scanReportProbe/cmx_handler.c
+++ src/scanReportProbe/cmx_handler.c
@@ -9,7 +9,9 @@ void init_parameters(){
 	CONFIG.URL="http://tribe.openia.it/MerakiCMXDataCollector/events?code=";
 	CONFIG.CODE="UNIPING";
 	CONFIG.VALIDATOR="f97ab62d-11fd-4f90-a352-e852ff684f4d";
-	CONFIG.PATH_CMX="Ezekiel25:17";//To be fixed
+	CONFIG.PATH_CMX="path_cmx/";//To be changed. TODO
+	CONFIG.SECRET="uniping-tribe";
+	CONFIG.MAC_AP="E4:95:6E:40:8D:2E";
 }
 
 /*This callback function is called as soon as possibile when data is completely received*/
@@ -27,43 +29,102 @@ static size_t write_callback(void *contents, size_t size, size_t nmemb, void *us
 int check_validator(){
 if(strcmp(validator,CONFIG.VALIDATOR)==0){
 	printf("The get validator is ok\n");
-	return 1;
+	return OK;
 }else{
 	printf("The get validator isn't ok\n");
-	return -1;
+	return KO;
 }	
 }
 
 /*It returns 1 if there is at least one file in the CONFIG.PATH, otherwise -1*/
 int file_presence(){
 
-return -1;
+	DIR *dir;
+	char *THIS_DIR=".";
+	char *PREVIOUS_DIR="..";
+	char *file_name;
+
+	CURL *curl;
+	CURLM *multi_handle;
+	static const char buf[] = "Expect:";
+	int still_running;
+
+	struct curl_httppost *formpost=NULL;
+  	struct curl_httppost *lastptr=NULL;
+  	struct curl_slist *headerlist=NULL;
+
+	int number_files=0;
+	
+	struct dirent *ent;
+
+	if ((dir = opendir (CONFIG.PATH_CMX)) != NULL) { 
+		/*While there are file in the folder, put in a HTTP POST to send list*/
+		while ((ent = readdir (dir)) != NULL) {
+			if(strcmp(ent->d_name,THIS_DIR)!=0 && strcmp(ent->d_name,PREVIOUS_DIR)!=0){
+				number_files++;
+				file_name=ent->d_name;
+				printf("Name file :%s\n",file_name);
+				curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "Pippo", CURLFORM_FILE, file_name, CURLFORM_END);
+				}
+		}
+
+	curl = curl_easy_init();
+	multi_handle = curl_multi_init();
+ 
+	/* initialize custom header list (stating that Expect: 100-continue is not wanted */ 
+	headerlist = curl_slist_append(headerlist, buf);
+	if(curl && multi_handle) {
+	 
+		char *url_post=get_auth_url();
+		/* what URL that receives this POST */
+		printf("URL POST:%s\n",url_post);
+
+		curl_easy_setopt(curl, CURLOPT_URL, url_post);
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerlist);
+		curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+		curl_easy_setopt(curl, CURLOPT_HTTPPOST, formpost);
+ 
+		curl_multi_add_handle(multi_handle, curl);
+		 
+		int res=curl_multi_perform(multi_handle, &still_running);
+		printf("%d\n",res);
+ 		if(res != CURLE_OK){
+			fprintf(stderr, "curl_multi_perform() failed: %s\n", curl_easy_strerror(res));
+			curl_easy_cleanup(curl);
+			
+		}
+
+	curl_multi_cleanup(multi_handle);/* always cleanup */ 
+	curl_easy_cleanup(curl);
+	curl_formfree(formpost);/* then cleanup the formpost chain */ 
+	curl_slist_free_all (headerlist); /* free slist */ 
+  }
+
+  closedir (dir);
+} else {
+  /* could not open directory */
+  perror ("");
+  return KO;
+}
+return OK;
 }
 
 
 /*It executes a HTTP POST on the server. It returns 1 if the post goes ok (in this case the corresponding file will be deleted), otherwise returns -1*/
-int post_cmx(){
-	if(file_presence()){
+int http_post_cmx(char *file_name){
+
 	
-	/*
-	For each file in the folder, make an HTTP POST
-		*/
 	return 1;
-	}
-	return -1;
 }
 
 /*Given in input the file name, it will be deleted*/
-void delete_file(char* name_file){
-
+int delete_file(char* name_file){
+	
+	return 1;
 }
 
-/*The validator is get from HTTP.. GET. This function returns 1 if the validator is get correctly, otherwise -1*/
-int 
-get_validator(){
-	CURL *curl;
-	CURLcode res;
-	
+/* This function simply concatenates URL and the CODE in the config parameters*/
+char* get_auth_url(){
 	size_t len_URL = strlen(CONFIG.URL);
 	size_t len_CODE = strlen(CONFIG.CODE);
 
@@ -72,10 +133,22 @@ get_validator(){
 	memcpy(URL_AUTH, CONFIG.URL, len_URL);
 	memcpy(URL_AUTH + len_URL, CONFIG.CODE, len_CODE + 1);
 
-	printf("Url Auth:%s\n",URL_AUTH);
+	//printf("Url Auth:%s\n",URL_AUTH);
+	return URL_AUTH;
+	
+}
+/*The validator is get from HTTP.. GET. This function returns 1 if the validator is get correctly, otherwise -1*/
+int 
+get_validator(){
+	CURL *curl;
+	CURLcode res;
+
+	char * URL_AUTH = get_auth_url();
+
 	curl = curl_easy_init();
 	if(curl){
 		curl_easy_setopt(curl, CURLOPT_URL, URL_AUTH);
+		curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);/* Verbose for more information*/
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
 
 		/* Perform the request, res will get the return code */ 
@@ -89,19 +162,27 @@ get_validator(){
 		fprintf(stderr, "curl_easy_perform() failed: %s\n",
 		curl_easy_strerror(res));
 		curl_easy_cleanup(curl);
-		return -1;
+		return KO;
 		}
 
     curl_easy_cleanup(curl);
   }
-  return 1;
+  return OK;
 }
 
 
 int main()
 {
+	int check_val=0;
 	init_parameters();
-	get_validator();	
-	check_validator();
-	return 0;
+
+	if(get_validator()==OK){	
+		check_val=check_validator();
+		}else return KO;
+
+	if(check_val==OK){
+		file_presence();
+		}else return KO;
+
+
 }
--- src/scanReportProbe/cmx_handler.h
+++ src/scanReportProbe/cmx_handler.h
@@ -3,6 +3,10 @@
 #include <sys/stat.h> 
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
+
+#define OK 100
+#define KO 404
 
 /*Data structure in which the parameters get from the file are stored*/
 struct config_parameters{
@@ -10,9 +14,12 @@ struct config_parameters{
 	char *CODE;/*Code by which is possible to get the validator*/
 	char *VALIDATOR;/*The local validator*/
 	char *PATH_CMX;/*The path in which the CMX files are stored*/
+	char *SECRET;/*The secret*/
+	char *MAC_AP;/*MAC address appliance. Maybe not needed as configuration parameters*/
 };
 
 /*Prototypes of the functions*/
 static size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp);
 int check_validator();
 int get_validator();
+char* get_auth_url();
