6491a25299b53e32eaa86176ec2d3882e4af61d1
FaGr <FaGr@users.noreply.github.com>
2016-09-11 18:50:15 +0200

    Update handler_config.h

--- src/scanReportProbe/handler_config.h
+++ src/scanReportProbe/handler_config.h
@@ -1,8 +1,8 @@
  
- typedef struct config_parameters {		char URL[58]		//URL of the server
-		                                char CODE[7];		//Code by which is possible to get the validator
-		                                char VALIDATOR[36];	//The local validator
-		                                char PATH_CMX[30];	//The path in which the CMX files are stored
- 		                                char SECRET[13]; 	//The secret 
-                                        	char MAC_AP[17];	//MacAddress of GL device
+ typedef struct config_parameters {		char *URL;		//URL of the server
+		                                char *CODE;		//Code by which is possible to get the validator
+		                                char *VALIDATOR;	//The local validator
+		                                char *PATH_CMX;		//The path in which the CMX files are stored
+ 		                                char *SECRET	 	//The secret 
+                                        	char *MAC_AP;		//MacAddress of GL device
                                    } scp;
