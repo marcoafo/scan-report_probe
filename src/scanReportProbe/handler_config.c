46640440b0b82583d2ac20e82b0ac5af18ca8386
FaGr <FaGr@users.noreply.github.com>
2016-09-11 19:49:38 +0200

    Update handler_config.c

--- src/scanReportProbe/handler_config.c
+++ src/scanReportProbe/handler_config.c
@@ -3,32 +3,73 @@
 #include <libconfig.h>
 #include "handler_config.h" 
 
-int init_parameters(){
-  config_t cfg;
-  config_init (&cfg); //initializes the config_t structure pointed to by config as a new,empty configuration
-  //config_setting_t *setting;
-  const char *str;
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE -1
+#define CONFIGFILE "scanReportProbe.conf"
 
-  /* Read the file. If there is an error, report it and exit. */
-  if(! config_read_file(&cfg, "scanReportProbe.conf"))
+#define GET_STRING_CONFIG_INTERFACE 0
+#define GET_STRING_CONFIG_PKG_TYPE 1
+#define GET_STRING_CONFIG_CURL_CONFIG 2
+
+extern char *INTERFACE=NULL;
+extern char *PKG_TYPE=NULL;
+extern scp SCP;
+
+
+int init_parameters(int varType, char *variableName){
+  config_t cfg;
+  config_setting_t *setting;
+  config_init (&cfg); //initializes the config_t structure pointed by config as a new,empty configuration
+  
+ 
+  // Read and parse the file scanReportProbe.conf into the configuration object cfg. If there is an error, report it and exit.
+  //It returns an int : CONFIG_TRUE on success, or CONFIG_FALSE on failure; the config_error_text() and config_error_line() functions, can be used to obtain information about the error. 
+  if(! config_read_file(&cfg, CONFIGFILE))
   {
     fprintf(stderr, "%s:%d - %s\n", config_error_file(&cfg),config_error_line(&cfg), config_error_text(&cfg));
     config_destroy(&cfg);
     return(EXIT_FAILURE);
   }
-  if(config_lookup_string(&cfg, "interface", &str))  { //assegnare il valore ad una variabile globale ?
-                                                      printf("Interface name : %s\n\n", str); }
-  else
-      fprintf(stderr, "No 'Interface' setting in configuration file scanReportProbe.conf.\n");
   
-  if(config_lookup_string(&cfg, "pkg_type", &str))  {  //assegnare il valore ad una variabile globale ?
-                                                      printf("pkg_type : %s\n\n", str); }
-  else
-      fprintf(stderr, "No 'pkg_type' setting in configuration file scanReportProbe.conf.\n");
- 
+  switch (varType) {
+        case (GET_STRING_CONFIG_INTERFACE):
+          setting = config_lookup(&cfg, INTERFACE);
+          if (setting == NULL) {fprintf(stderr, "No 'Interface' setting in configuration file scanReportProbe.conf.\n");
+                                config_destroy(&cfg);
+                                return (EXIT_FAILURE);
+                                }
+          else  {(config_lookup_string(&cfg, "interface", &INTERFACE));
+                 printf("Interface name : %s\n\n", INTERFACE); 
+                 config_destroy(&cfg);
+                 return (EXIT_SUCCESS);
+                };
+        break;
+        
+        case (GET_STRING_CONFIG_PKG_TYPE):
+          setting = config_lookup(&cfg, PKG_TYPE);
+          if (setting == NULL){fprintf(stderr, "No 'pkg_type' setting in configuration file scanReportProbe.conf.\n");
+                               config_destroy(&cfg);
+                               return (EXIT_FAILURE);
+                               }
+          else {(config_lookup_string(&cfg, "pkg_type", &PKG_TYPE)); 
+                printf("pkg_type : %s\n\n", PKG_TYPE); 
+                config_destroy(&cfg);
+                return (EXIT_SUCCESS);
+               };
+        break;
+        
+        case (GET_STRING_CONFIG_CURL_CONFIG):
+        setting = config_lookup(&cfg, &SCP);
+        if (setting == NULL){fprintf(stderr, "No 'config_parameters_structure' setting in configuration file scanReportProbe.conf.\n");
+                               config_destroy(&cfg);
+                               return (EXIT_FAILURE);
+                               }
+          else {
+  
+  
+  
+        break;
  /*
- if ( config_read_file (config_t * config, const char * scanReportProbe.conf) == "CONFIG_TRUE"){  //config_read_file reads and parses a configuration from the file named scanReportProbe.conf into the configuration object config. 
- //It returns an int : CONFIG_TRUE on success, or CONFIG_FALSE on failure; the config_error_text() and config_error_line() functions, can be used to obtain information about the error. 
  scp = {
       scp.URL= *config.URL,
       scp.CODE= *config.CODE,
